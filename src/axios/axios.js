9106eb271cafccfb7bd51a28443e6ad0f0b118ec
Ashraful Alam <mdmashraful1998@gmail.com>
2022-10-17 03:08:09 +0600

    error handeling and get put delete post request applied

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -6,9 +6,25 @@ import axios from 'axios';
 
 class Axios extends Component {
     componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        //by using axios.get method to show data or fetch information form another server
+        axios.get('http://localhost:3001/dishes')
             .then(response => response.data)
-            .then(data => console.log("From Axios:", data));
+            .then(data => console.log("From Axios:", data))
+            .catch(error => console.log(error.message));
+        //by using axios.post method we can manipulat infromation or add information in server
+        axios.post('http://localhost:3001/dishes', { name: "Garlic Pizza with Cheese", price: "220" })
+            .then(response => response.data)
+            .then(data => console.log(data));
+
+        //By using this axios.put,change information from server
+        axios.put('http://localhost:3001/dishes/1', { name: "Chicken Burger", price: "350" })
+            .then(resopnse => console.log(resopnse));
+
+        //by using axios.delete, request to the server for delete data form server 
+        axios.delete('http://localhost:3001/dishes/0')
+            .then(response => console.log(response));
+
+
     }
     render() {
         return (
