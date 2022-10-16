edc55c1692c359507d9f4c012ec89d2f588db8e0
Ashraful Alam <mdmashraful1998@gmail.com>
2022-10-17 01:04:51 +0600

    .

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Welcome To RESRApi </h1>
+      <div className='App'>
+        <h1>Welcome To RESTAPI </h1>
       </div>
     );
   }
